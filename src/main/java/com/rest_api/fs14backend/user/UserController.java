fabbeaed035c74d3dbdff5f3f3159d1375a6f4fd
Mahjabeen Akter <mahjabeen.sust@gmail.com>
2023-05-17 21:22:44 +0300

    username and password error during signin

--- src/main/java/com/rest_api/fs14backend/user/UserController.java
+++ src/main/java/com/rest_api/fs14backend/user/UserController.java
@@ -2,8 +2,11 @@ package com.rest_api.fs14backend.user;
 
 import com.rest_api.fs14backend.utils.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,17 +35,39 @@ public class UserController {
   @PostMapping("/signin")
   public String login(@RequestBody AuthRequest authRequest){
 
-    authenticationManager.authenticate(
+    //original code from yazan
+    /*authenticationManager.authenticate(
       new UsernamePasswordAuthenticationToken(
         authRequest.getUsername(),
         authRequest.getPassword()
       )
     );
     
-    
     User user = userRepository.findByUsername(authRequest.getUsername());
     //System.out.println("found logged in user : " +user);
-    return jwtUtils.generateToken(user);
+    return jwtUtils.generateToken(user);*/
+    //original code from yazan ends
+   
+    
+    
+    User user = userRepository.findByUsername(authRequest.getUsername());
+    if (user == null) {
+      //System.out.println(HttpStatus.NOT_FOUND);
+      return HttpStatus.NOT_FOUND.toString();
+    } else {
+      
+      if(!passwordEncoder.matches(authRequest.getPassword(), user.getPassword())){
+        return HttpStatus.UNAUTHORIZED.toString();
+      }
+      authenticationManager.authenticate(
+          new UsernamePasswordAuthenticationToken(
+              authRequest.getUsername(),
+              authRequest.getPassword()
+          )
+      );
+     
+      return jwtUtils.generateToken(user);
+    }
     
   }
 
